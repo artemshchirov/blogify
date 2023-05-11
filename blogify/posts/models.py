cfaa9c90dcd383dae000dbfe4e97f4fd60771b74
Artem Shchirov <artemshchirov@gmail.com>
2023-05-11 23:09:49 +0300

    feat(server): update db, update views for new content, add post model

--- blogify/posts/models.py
+++ blogify/posts/models.py
@@ -1,3 +1,10 @@
-from django.db import models
+from django.db.models import Model, TextField, DateTimeField, ForeignKey, CASCADE
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Post(Model):
+    text = TextField()
+    pub_date = DateTimeField(auto_now_add=True)
+    author = ForeignKey(User, on_delete=CASCADE, related_name='posts')
--- blogify/posts/views.py
+++ blogify/posts/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 
+from . models import Post
+
 
 def index(request):
     template = 'posts/index.html'
     title = 'Blogify'
+    posts = Post.objects.order_by('-pub_date')[:10]
     context = {
         'title': title,
+        'posts': posts
     }
     return render(request, template, context)
 
@@ -13,7 +17,9 @@ def index(request):
 def group_posts(request):
     template = 'posts/group_list.html'
     title = 'Blogify - posts'
+    posts = Post.objects.order_by('-pub_date')[:10]
     context = {
         'title': title,
+        'posts': posts
     }
     return render(request, template, context)
